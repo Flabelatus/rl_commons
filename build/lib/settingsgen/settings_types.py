73986db98e4c2283d7e42149d1d6636c347e9d4e
Flabelatus <jooshesh.javid@gmail.com>
2025-07-07 20:12:04 +0200

    Initial commit

--- build/lib/settingsgen/settings_types.py
+++
@@ -1,9 +0,0 @@
-"""
-The dataclasses are automatically generated from the given yaml schema
-"""
-from dataclasses import dataclass
-from typing import List, Optional, Any
-
-@dataclass
-class RootSchema:
-    mode: str
